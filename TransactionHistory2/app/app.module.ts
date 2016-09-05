0d23bdfbaa0dce3198ac5e104e7b7092a336b03d
TWood <tomwood2@gmail.com>
2016-09-05 15:36:22 -0700

    Initial version

---
+++ TransactionHistory2/app/app.module.ts
@@ -0,0 +1,21 @@
+﻿import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+
+import { TransactionHistoryComponent }  from './transaction-history.component';
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        TransactionHistoryComponent
+    ],
+    //providers: [
+    //    HeroService
+    //],
+    bootstrap: [
+        TransactionHistoryComponent
+    ]
+
+})
+export class AppModule { }
---
+++ TransactionHistory2/app/transaction-history.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core'
+
+@Component({
+    selector: 'transaction-history',
+    templateUrl: 'app/views/transaction-history.component.html',
+    styleUrls: ['app/styles/transaction-history.component.css']
+})
+export class TransactionHistoryComponent {
+    title = 'Transaction History';
+}
